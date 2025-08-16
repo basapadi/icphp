d9db7e80a4fd120864c548d7bd2dee86c9c82e7c
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2025-08-16 15:34:34 +0700

    moving initial menus from seeder to config

--- config/ihandcashier.php
+++ config/ihandcashier.php
@@ -1,4 +1,51 @@
 <?php
 return [
-    'roles' => ['admin','kasir'] //available role
+    'roles' => ['admin','kasir'], //available role,
+    'menus' => [
+        [
+            'id'    => 1,
+            'icon'  => 'Home',
+            'label' => 'Beranda',
+            'route' => '/',
+            'order' => 1,
+        ],
+        [
+            'id'    => 2,
+            'icon'  => 'CreditCard',
+            'label' => 'POS',
+            'route' => '/pos',
+            'order' => 2
+        ],
+        [
+            'id'    => 3,
+            'icon'  => 'Settings',
+            'label' => 'Pengaturan',
+            'route' => '#',
+            'order' => 3
+        ],
+        [
+            'id'    => 4,
+            'icon'  => '',
+            'label' => 'Umum',
+            'route' => '/setting/general',
+            'order' => 0,
+            'parent'=> 3
+        ],
+        [
+            'id'    => 5,
+            'icon'  => '',
+            'label' => 'Basis Data',
+            'route' => '/setting/database',
+            'order' => 1,
+            'parent'=> 3
+        ],
+        [
+            'id'    => 6,
+            'icon'  => '',
+            'label' => 'Hak Akses',
+            'route' => '/setting/roles',
+            'order' => 2,
+            'parent'=> 3
+        ]
+    ]
 ];
--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -12,45 +12,7 @@ class MenuSeeder extends Seeder
      */
     public function run(): void
     {
-        $menus = [
-            [
-                'id'    => 1,
-                'icon'  => 'Home',
-                'label' => 'Beranda',
-                'route' => '/',
-                'order' => 1,
-            ],
-            [
-                'id'    => 2,
-                'icon'  => 'CreditCard',
-                'label' => 'POS',
-                'route' => '/pos',
-                'order' => 2
-            ],
-            [
-                'id'    => 3,
-                'icon'  => 'Settings',
-                'label' => 'Pengaturan',
-                'route' => '#',
-                'order' => 3
-            ],
-            [
-                'id'    => 4,
-                'icon'  => '',
-                'label' => 'Umum',
-                'route' => '/setting/general',
-                'order' => 0,
-                'parent'=> 3
-            ],
-            [
-                'id'    => 5,
-                'icon'  => '',
-                'label' => 'Basis Data',
-                'route' => '/setting/database',
-                'order' => 1,
-                'parent'=> 3
-            ]
-        ];
+        $menus = config('ihandcashier.menus');
 
         \DB::beginTransaction();
         Menu::truncate();
