17a10722d1c0ca9c602db528a103452be0883dee
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2025-08-26 16:36:24 +0700

    Dashboard:: Membuat instruksi pengembangan halamana dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,31 +16,38 @@ class DashboardController extends BaseController
     public function data(Request $request){
         $this->allowAccessModule('dashboard','view');
         
-        //TODO:: ganti value dibawah dengan real value
+        //TODO:: ganti value cards dan grids dibawah dengan real value
         $cards = [
             [
-                'title' => 'Total Penjualan Bulan '.date('M'),
+                'title' => 'Total Penjualan Bulan '.date('M'), //total nilai penjualan barang periode bulan berjalan (bruto)
                 'value' => 'Rp.157.002.000',
                 'change' => '+2.05%',
                 'trend' => 'up',
                 'icon' => 'DollarSign'
             ],
             [
-                'title' => 'Total Pengeluaran Bulan '.date('M'),
-                'value' => 'Rp.57.002.000',
-                'change' => '-2.05%',
-                'trend' => 'down',
+                'title' => 'Total Laba Bersih Penjualan '.date('M'), //total nilai netto (harga jual - harga beli) dari semua brang terjual bulan berjalan
+                'value' => 'Rp.57.579.000',
+                'change' => '+1.05%',
+                'trend' => 'up',
                 'icon' => 'DollarSign'
             ],
+            // [
+            //     'title' => 'Total Pengeluaran Bulan '.date('M'), //ini diskip karena belum ada modul transaksi pengeluaran
+            //     'value' => 'Rp.0',
+            //     'change' => '0%',
+            //     'trend' => '-',
+            //     'icon' => 'DollarSign'
+            // ],
             [
-                'title' => 'Total Piutang Bulan '.date('M'),
+                'title' => 'Total Piutang Bulan '.date('M'), //ini diskip, tunggu modul piutang selesai kita buat
                 'value' => 'Rp.25.970.000',
                 'change' => '+1.09%',
                 'trend' => 'up',
                 'icon' => 'DollarSign'
             ],
             [
-                'title' => 'Total Hutang Bulan '.date('M'),
+                'title' => 'Total Hutang Bulan '.date('M'), // ini diskip, tunggu selesai modul hutang kita buat
                 'value' => 'Rp.15.240.000',
                 'change' => '+1.09%',
                 'trend' => 'up',
@@ -49,13 +56,13 @@ class DashboardController extends BaseController
         ];
 
         $grids = [
-            'receive_duedate' => [
+            'receive_duedate' => [ //listkan semua penerimaan barang yang type pembayarannya adalah tempo (Hutang) 2 minggu kedepan
                 ['kode_transaksi' => 'ttt','contact_name' => 'test', 'tanggal_duedate' => '2025/08/26', 'syarat' => '2/15 N30']
             ],
-            'sale_duedate' => [
+            'sale_duedate' => [ //listkan semua penjualan barang yang type pembayarannya adalah tempo (Hutang) 2 minggu kedepan
                 ['kode_transaksi' => 'ttt','contact_name' => 'test', 'tanggal_duedate' => '2025/08/26', 'syarat' => '2/15 N30']
             ],
-            'top_products' => [
+            'top_products' => [ //listkan 10 barang paling laku selama bulan ini
                 ['nama_barang' => 'ttt','sales' => 'test', 'revenue' => '15.000.000']
             ]
         ];
