1fde4686a824eab6af1b416ce581f93834c9d1f0
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2025-08-26 10:44:14 +0700

    Perbaikan DashboardGrid

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -1,22 +1,46 @@
 <template>
   <AdminLayout>
     <div class="max-w-full">
-      <div class="mb-2 bg-gray-50 border-b border-gray-200 p-2">
+      <div class="bg-gray-50 border-b border-gray-200 p-2">
         <h1 class="text-lg font-bold text-gray-900 mb-0">Dashboard</h1>
         <p class="text-xs text-gray-600">Selamat datang kembali, apa yang terjadi pada bisnis anda hari ini?</p>
       </div>
 
       <!-- Dashboard Content -->
       <div class="p-2">
-        <DashboardCards />
+        <DashboardCards :stats="data.cards"/>
         <DashboardGrid />
       </div>
     </div>
   </AdminLayout>
 </template>
 
-<script setup>
+<script>
 import AdminLayout from '@/layouts/AdminLayout.vue'
 import DashboardCards from '@/components/DashboardCards.vue'
 import DashboardGrid from '@/components/DashboardGrid.vue'
+
+export default {
+  name: "Dashboard",
+  components: {
+    AdminLayout,
+    DashboardCards,
+    DashboardGrid
+  },
+  data() {
+    return {
+      data: {},
+    };
+  },
+  methods: {
+    async load() {
+      await this.$store.dispatch('dashboard/data').then(({ data }) => {
+        this.data = data.data;
+      })
+    }
+  },
+  beforeMount() {
+    this.load()
+  }
+}
 </script>
