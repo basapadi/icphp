c81e6b216fdc38d07c240eae53466be389f3e92c
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2025-09-12 09:29:09 +0700

    Demo Version

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,34 @@
 <template>
   <div id="app">
     <vue3-confirm-dialog></vue3-confirm-dialog>
+    <div class="watermark shadow-lg">
+      Demo Version
+    </div>
     <router-view />
   </div>
 </template>
+<style>
+  .watermark {
+    position:fixed;
+    bottom:20px;
+    left:35px;
+    opacity:1;
+    z-index:9999;
+    font-size: 1.2em;
+    font-weight: 800;
+    margin: 5px;
+    border-radius: 5px;
+    border-width: 1;
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    text-shadow: 1em;
+    background-color: rgb(247, 132, 0);
+    font-family:'monospace';
+    color:rgb(255, 255, 255);
+  }
+</style>
 <script setup>
 import { onMounted, onBeforeUnmount } from 'vue'
 
