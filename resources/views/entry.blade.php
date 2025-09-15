e1dbd1f34f7915deeee0cfe46ac4c496c99c2ab6
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2025-09-15 09:24:31 +0700

    fix: Update meta tag

--- resources/views/entry.blade.php
+++ resources/views/entry.blade.php
@@ -4,12 +4,11 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/ihand-512.png" />
         <link rel="apple-touch-icon"  href="/ihand-512.png" />
-        <meta name="description" content="Simple POS Ihand Cashier Mobile and Desktop for your business" />
+        <meta name="description" content="POS Single Entry Multiplatform App. Tersedia versi build Windows, MacOS dan Linux" />
         <link rel="canonical" href="https://ihandcashier.basapadi.com" />
-        <title>IhandCashier</title>
+        <title>Ihand Cashier</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
