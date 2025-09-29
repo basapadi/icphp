153ccba17bbabdcc3cfb752ef1e37912dd40de67
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2025-09-29 07:52:14 +0700

    feat: Change log UI

---
+++ app/Http/Controllers/CommonController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Http\Response;
+class CommonController extends Controller
+{
+    public function ChangeLog(Request $request){
+        $owner = 'basapadi';
+        $repo = 'icphp';
+        $token = env('GITHUB_TOKEN');
+        $branch = env('GITHUB_BRANCH');
+
+        $uri = "https://api.github.com/repos/{$owner}/{$repo}/commits?sha={$branch}&per_page=10";
+
+        $response = Http::withHeaders([
+            'Authorization' => "token ${token}",
+            'Accept'        => 'application/vnd.github.v3+json',
+            'User-Agent'    => 'Ihand-Cashier',
+        ])->get($uri);
+
+        if($response->failed()){
+            Response::badRequest('Github API Error');
+        }
+
+        $commits = collect($response->json())->map(function($item){
+            return [
+                'message'   => $item['commit']['message'],
+                'author'    => $item['commit']['author']['name'],
+                'date'      => $item['commit']['author']['date']
+            ];
+        });
+
+        return Response::ok('Loaded', $commits);
+    }
+}
---
+++ resources/js/components/ChangeLog.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="max-w-2xl mx-2 p-2">
+    <h2 class="text-xl mb-4 text-gray-600">Histori perubahan aplikasi terakhir</h2>
+
+    <!-- Loading State -->
+    <div v-if="loading" class="text-gray-500 text-center">Memuat commits...</div>
+
+    <!-- Error State -->
+    <div v-else-if="error" class="text-red-600 text-center">{{ error }}</div>
+
+    <!-- Commit List -->
+    <ul v-else class="space-y-2 max-h-84 overflow-y-auto">
+      <li v-for="(commit, index) in commits" :key="index" class="p-2 border border-dashed rounded-lg bg-white shadow-sm hover:shadow-md transition">
+        <p class="text-gray-600 font-semibold">
+          {{ commit.message }}
+        </p>
+        <div class="mt-1 text-sm text-gray-600 flex justify-between">
+          <span class="italic text-xs text-muted-foreground">by {{ commit.author }}</span>
+          <span class="italic text-xs">{{ formatDate(commit.date) }}</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const commits = ref([])
+const loading = ref(true)
+const error = ref(null)
+
+const fetchCommits = async () => {
+  try {
+    const { data } = await axios.get('/api/auth/change-log')
+    console.log(data)
+    commits.value = data.data
+  } catch (e) {
+    error.value = 'Gagal memuat commits'
+  } finally {
+    loading.value = false
+  }
+}
+
+const formatDate = (isoString) => {
+  const d = new Date(isoString)
+  return d.toLocaleString('id-ID', {
+    dateStyle: 'medium',
+    timeStyle: 'short'
+  })
+}
+
+onMounted(fetchCommits)
+</script>
