2624c01a0b5ba4c96f7a9bf55427277d645cbf30
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2025-10-21 10:40:05 +0700

    fix: NativeAppServiceProvider

--- app/Providers/NativeAppServiceProvider.php
+++ app/Providers/NativeAppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Native\Desktop\Facades\{Window,Settings};
-use Native\Desktop\Contracts\ProvidesPhpIni;
+use Native\Laravel\Facades\{Window,Settings};
+use Native\Laravel\Contracts\ProvidesPhpIni;
 use Illuminate\Support\Facades\Artisan;
 
 class NativeAppServiceProvider implements ProvidesPhpIni
