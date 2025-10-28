5b33868a631af12847827a04a99302d3d68ec789
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2025-10-28 09:05:01 +0700

    Meta Description Update

--- resources/views/entry.blade.php
+++ resources/views/entry.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
         <link rel="apple-touch-icon"  href="/icon.png" />
-        <meta name="description" content="Simple POS Multiplatform App. Tersedia versi build Windows, MacOS dan Linux" />
+        <meta name="description" content="Ihand Cashier adalah aplikasi kasir (POS) serbaguna untuk membantu UMKM dan bisnis retail mengelola transaksi, stok, pelanggan, dan laporan penjualan secara cepat dan efisien. Didesain modular dan fleksibel, Ihand Cashier mendukung sistem plugin untuk pengembangan fitur sesuai kebutuhan bisnis Anda.Tersedia versi build Windows, MacOS dan Linux">
         <link rel="canonical" href="https://ihandcashier.basapadi.com" />
         <title>Ihand Cashier</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -14,8 +14,8 @@
         <!-- <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"> -->
         <!-- <link href="ia-writer-quattro-latin-400-normal.ttf" rel="stylesheet"> -->
 
-        <meta property="og:title" content="Simple POS Multiplatform App. Tersedia versi build Windows, MacOS dan Linux - Ihand Cashier">
-        <meta property="og:description" content="Aplikasi yang dapat membantu mengelola bisnis anda">
+        <meta property="og:title" content="Ihand Cashier - Simple POS Multiplatform App. Tersedia versi build Windows, MacOS dan Linux">
+        <meta property="og:description" content="Ihand Cashier adalah aplikasi kasir (POS) serbaguna untuk membantu UMKM dan bisnis retail mengelola transaksi, stok, pelanggan, dan laporan penjualan secara cepat dan efisien. Didesain modular dan fleksibel, Ihand Cashier mendukung sistem plugin untuk pengembangan fitur sesuai kebutuhan bisnis Anda. Ihand Cashier juga tersedia versi build Windows, MacOS dan Linux">
         <meta property="og:type" content="website"> <!-- article jika konten artikel -->
         <meta property="og:url" content="https://ihandcashier.basapadi.com">
         <meta property="og:site_name" content="Ihand Cashier">
